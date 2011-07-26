c72c6803350140d1eac89225d9de9bba4449cf4b
Kupker <kupker@gmail.com>
2011-07-26 11:00:28 +0200

    Frightened Citizens have now waypoints saved in DB

---
+++ sql/blizzlike/updates/2011_07_26_waypoint_data.sql
@@ -0,0 +1,20 @@
+-- Frightened Citizen, Waypoints to Quest Evacuate the Merchant Square
+
+DELETE FROM waypoint_data WHERE id in(349810, 349811);
+INSERT INTO `waypoint_data` VALUES (349810, 1, -1544.83, 1429.68, 35.5559, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349810, 2, -1554.44, 1409.34, 35.5559, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349810, 3, -1554.34, 1388.02, 35.5861, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349810, 4, -1557.97, 1361.57, 35.5586, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349810, 5, -1560.59, 1333.97, 35.5571, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349810, 6, -1568.32, 1327.29, 35.5571, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349810, 7, -1577.35, 1317.59, 34.9365, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349811, 1, -1463.96, 1429.41, 35.5573, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349811, 2, -1429.19, 1422.41, 35.5573, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349811, 3, -1419.33, 1419.23, 35.5573, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349811, 4, -1406.9, 1416.03, 35.5573, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349811, 5, -1403.28, 1401.21, 35.5573, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349811, 6, -1407.48, 1375.36, 35.5573, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349811, 7, -1502.08, 1342.68, 35.5574, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349811, 8, -1537.7, 1330.3, 35.5574, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349811, 9, -1562.15, 1319.15, 35.5574, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (349811, 10, -1577.96, 1316.79, 34.497, 0, 0, 0, 100, 0);
